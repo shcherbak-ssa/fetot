2a28098c8ad5d90a2aecc122c302f89c6630c84a
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2019-10-12 10:25:12 +0300

    Client: refactore login mode DRAFT

--- build/login/index.js
+++ build/login/index.js
@@ -5,6 +5,8 @@
 import Vue from 'vue/dist/vue';
 import 'fetot-main-scss';
 
+import fetchRequest from 'fetot-network-modules/fetch-request';
+
 import initApplication from './src/app-init';
 import fetotLogin from './login.vue';
 
@@ -12,13 +14,23 @@ import fetotLogin from './login.vue';
 /*** init [begin] ***/
 
 initApplication()
-	.then((options) => {
-		new Vue({
-			el: '#login',
-			data: { options },
-			template: '<fetot-login :options="options"/>',
-			components: { 'fetot-login': fetotLogin }
-		})
-	});
+	.then(createVueModel)
+	.then(fetchRequest.connection);
 
 /*** init [end] ***/
+/*** src [begin] ***/
+
+async function createVueModel(options) {
+	new Vue({
+		el: '#login',
+		data: { options },
+		template: '<fetot-login :options="options"/>',
+		components: { 'fetot-login': fetotLogin }
+	});
+	return {
+		currentMode: 'login',
+		currentModule: options.store.get('current-module')
+	}
+}
+
+/*** src [end] ***/
