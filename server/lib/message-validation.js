db4c8e1c71b1aa99ffc9c29871a88e24f5905f51
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2019-10-12 20:07:01 +0300

    Server: validation input message

--- server/lib/message-validation.js
+++ server/lib/message-validation.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/*** init [begin] ***/
+
 const validMessageType = ['connection', 'message', 'change-mode', 'change-module'],
 	validMessage = {
 		type: String,
@@ -11,52 +13,56 @@ const validMessageType = ['connection', 'message', 'change-mode', 'change-module
 		data: Object
 	};
 
+/*** init [end] ***/
 /*** exports [begin] ***/
 
 async function messageValidation(message) {
-	let messageMap = new Map(Object.entries(message)),
+	let messageMap = Object.entries(message),
 		valid = await validation(generatorMessageValidation(messageMap));
 	
 	return valid ? message : false;
 }
 
 /*** exports [end] ***/
+/*** src [begin] ***/
 
 async function validation(generator) {
-	let {value, done} = generator.next();
-	
-	if( done ) return value;
-	return value ? validation(generator) : false;
-}
-async function* generatorMessageValidation(message) {
-	if( message.size !== 3 ) yield false;
-	
-	yield await checkObject(message, validMessage);
-	
-	let messageContent = new Map(Object.entries(message.get('content')));
-	yield await checkObject(messageContent, validMessageContent);
-	
-	return await checkValidTypeValue(message.get('type'));
-}
-async function checkObject(messageObject, validObject) {
 	try {
-		messageObject.entries().map(async ([key, value]) => {
-			if( !validObject.has(key) ) throw 0;
-			
-			let valid = await checkValueType(value, validObject.get(key));
-			if( !valid ) throw 0;
-		});
-		return true;
-	} catch( e ) {
-		console.log(e);
+		let {value, done} = await generator.next();
+		
+		if( done ) return value;
+		return value ? validation(generator) : false;
+	} catch( err ) {
+		console.log(err);
 		return false;
 	}
 }
-async function checkValueType(messageValue, validValue) {
+async function* generatorMessageValidation(message) {
+	if( message.length !== 3 ) return Promise.reject();
+	
+	yield await checkObject(message, validMessage);
+	
+	let messageContent = Object.entries(message[2][1]);
+	yield await checkObject(messageContent, validMessageContent);
+	
+	return checkValidTypeValue(message[0][1]);
+}
+async function checkObject(message, validMessage) {
+	return Promise.all(message.map(([key, value]) => {
+		if( key in validMessage ) {
+			let valid = checkValueType(value, validMessage[key]);
+			if( !valid ) return Promise.reject(new Error('invalid value type'));
+		} else return Promise.reject(new Error('invalid key'));
+	}))
+}
+
+function checkValueType(messageValue, validValue) {
 	return typeof messageValue === typeof validValue()
 }
-async function checkValidTypeValue(messageType) {
+function checkValidTypeValue(messageType) {
 	return validMessageType.includes(messageType)
 }
+
+/*** src [end] ***/
 
 module.exports = messageValidation;
--- server/src/connection-events-handlers/http-connection-handler.js
+++ server/src/connection-events-handlers/http-connection-handler.js
@@ -7,14 +7,11 @@ const parseInputMessage = require('../../lib/parse-input-message'),
 /*** exports [begin] ***/
 
 async function httpConnectionHandler(request, response) {
-	console.log('connection init');
 	let message = await parseInputMessage({type: 'post-message', request});
-	console.log(message);
 	message = await messageValidation(message);
 	
 	if( !message ) return response.end('Message error');
 	
-	console.log('connection end');
 	response.end(JSON.stringify({message: {client: 1}}));
 	// await ClientWorker.clientConnection(message, response);
 }
