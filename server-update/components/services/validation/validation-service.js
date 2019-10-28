1b73ecf68e1c0a74eeafce68d07b5fae2e769b1b
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2019-10-28 10:57:26 +0300

    Server: validation-service READY

--- server-update/components/services/validation/validation-service.js
+++ server-update/components/services/validation/validation-service.js
@@ -1,2 +1,24 @@
 'use strict';
 
+/*** imports [begin] ***/
+
+const ValidateSchema = require('validate');
+
+/*** imports [end] ***/
+/*** exports [begin] ***/
+
+async function validation(schema, object) {
+	schema = await createSchema(schema);
+	return schema.validate(object);
+}
+
+/*** exports [end] ***/
+/*** src [begin] ***/
+
+async function createSchema(schema) {
+	return new ValidateSchema(schema);
+}
+
+/*** src [end] ***/
+
+module.exports = validation;
